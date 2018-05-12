f14b46d6e72448aebd86d19b7e36cf3ebb248988
Hannah <hannah_farisha@ymail.com>
2018-05-12 09:25:56 +0800

    up

--- config/database.php
+++ config/database.php
@@ -48,10 +48,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host'      => $host,
-            'database'  => $database,
-            'username'  => $username,
-            'password'  => $password,
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'fyp'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
