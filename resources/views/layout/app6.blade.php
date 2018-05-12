355aa135e7860a38c406fb246fe9098c6f901165
Hannah <hannah_farisha@ymail.com>
2018-05-12 10:31:47 +0800

    up

--- resources/views/layout/app6.blade.php
+++ resources/views/layout/app6.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=1500", "length=2000", initial-scale="1",maximum-scale="1",user-scalable="no">
+        <meta name="viewport" content="length=2000", initial-scale="1",maximum-scale="1",user-scalable="no">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="HandheldFriendly" content="true">
         <title>MSU Student's Activity Management System</title>
